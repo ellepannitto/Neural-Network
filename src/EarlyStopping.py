ab2b3f8eac51d26b894fc9fbaef0b3e5110f34cd
Lucio Messina <luciomessina94@gmail.com>
2018-01-26 15:31:51 +0100

    ver 0.2.3

--- src/EarlyStopping.py
+++ src/EarlyStopping.py
@@ -1,6 +1,10 @@
 
 import Params
+import Monk
 import numpy as np
+import OneHotEncoder
+from NeuralNetwork import NeuralNetwork
+import matplotlib.pyplot as plt
 
 class EarlyStopping:
 	
@@ -14,18 +18,19 @@ class EarlyStopping:
 		
 		self.layers_size = layers_size
 		
-	#TODO: moch: find a better implementation
+	#TODO: maybe it can be otpimized :)
 	def find_best_epoch ( self, losses ):
 		WINDOW = 5
+		TOLLERANCE = 0.001
 		for i in range (WINDOW, Params.MAX_EPOCH):
-			if all ([ losses[i]>losses[j] for j in range(i-WINDOW, i) ] ):
+			if all ([ losses[j] < losses[i] for j in range(i-WINDOW, i) ] ) or losses[i-WINDOW]<losses[i]+TOLLERANCE:
 				return np.argmin (losses[:i-WINDOW])
 		
 		return np.argmin (losses)
 		
 	def perform (self, do_plots = False):
 		
-		print ("*** Model {}: ***".format (self.model_name))
+		print ("*** Model: {} ***".format (self.model_name))
 		
 		stats = []
 		for i in range (Params.NUM_TRIALS_PER_CONFIGURATION):
@@ -63,10 +68,20 @@ class EarlyStopping:
 			stats.append ((when_to_stop, accuracy_per_epoch[when_to_stop]))
 		
 		self.mean_epochs = np.average([e[0] for e in stats])
-		self.var_epochs = np.var([e[0] for e in stats])
+		self.var_epochs = np.std([e[0] for e in stats])
 		self.mean_accuracy = np.average([e[1] for e in stats])
-		self.var_accuracy = np.var([e[1] for e in stats])
-		print ("mean: epochs {} +/- {} accuracy {} +/- {}".format( self.mean_epochs, self.var_epochs, self.mean_accuracy, self.car_accuracy) ) 
+		self.var_accuracy = np.std([e[1] for e in stats])
+		print ("mean: epochs {:.4f} +/- {:.4f} accuracy {:.4f} +/- {:.4f}".format( self.mean_epochs, self.var_epochs, self.mean_accuracy, self.var_accuracy) ) 
 	
 if __name__ == "__main__":
 	
+	train_s = Monk.monk3_training_set
+	train_l = Monk.monk3_training_labels
+	test_s  = Monk.monk3_test_set
+	test_l  = Monk.monk3_test_labels
+	
+	encoded_train_s = OneHotEncoder.encode_int_matrix (train_s)
+	encoded_test_s = OneHotEncoder.encode_int_matrix (test_s)
+	
+	es = EarlyStopping (encoded_train_s, train_l, encoded_test_s, test_l, layers_size=(2,), model_name="Monk 3")
+	es.perform (do_plots=True)
